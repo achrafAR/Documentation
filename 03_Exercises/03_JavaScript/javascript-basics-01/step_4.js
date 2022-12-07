38bf2e027f2b309d7c576a43d5caded3bd4d0744
achrafAR <achraf.alrachini@gmail.com>
2022-12-07 13:23:11 +0200

    js1/step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+function showalert(){
+    var user = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var city = document.getElementById("city").value;
+    alert (user+"\n"+ surname+"\n" + city);
+}
