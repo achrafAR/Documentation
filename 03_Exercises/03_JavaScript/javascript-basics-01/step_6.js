72d1bc0858c38acad83153904b5f4b8a33d9c162
achrafAR <achraf.alrachini@gmail.com>
2022-12-07 14:40:22 +0200

    js1/step6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_6.js
@@ -0,0 +1,5 @@
+function divide(){
+    var fnumber = document.getElementById("first_number").value;
+    var snumber = document.getElementById("second_number").value;
+    alert(fnumber/snumber);
+    }
