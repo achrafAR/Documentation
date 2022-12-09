23c59d139577ee890a9ba403e28630589c47772e
achrafAR <achraf.alrachini@gmail.com>
2022-12-09 11:08:04 +0200

    js3/step2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/js3step2.js
@@ -0,0 +1,11 @@
+function myfunction(x){
+    console.log(x)
+    var button=document.getElementById("x");
+    var p=document.getElementById("texte");
+    if(x=="show"){
+        p.style.display="block"
+    }else{
+        p.style.display="none"
+    }
+   
+}
