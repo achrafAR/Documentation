716d8a5b56063eee8b01ec295b88c4e86e65f703
achrafAR <achraf.alrachini@gmail.com>
2023-01-12 01:15:28 +0200

    Create an express simple API

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.listen(3000);
+
 app.get('/', function(req, res){
     res.send('ok')
 })
-app.listen(3000);
+
+app.get('/test', function(req, res){
+    res.status(200).json({status:200, message:"ok"});
+})
+
+app.get('/time', function(req, res){
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    res.status(200).json({status:200, message:`${hour}:${minutes}`});
+    
+});
+
